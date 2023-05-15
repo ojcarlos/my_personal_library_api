df7dec692b433de90550fb84032e92e4b96a0133
ojcarlos <carlos.ojunior00@gmail.com>
2023-05-15 16:22:46 -0400

    h2 and jpa added

--- personalLibrary/src/main/java/com/ojcarlos/personalLibrary/entities/User.java
+++ personalLibrary/src/main/java/com/ojcarlos/personalLibrary/entities/User.java
@@ -1,15 +1,17 @@
 package com.ojcarlos.personalLibrary.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.Objects;
 
-//@Entity
-//@Table(name ="tb_user")
+@Entity
+@Table(name ="tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nickname;
     private String password;
